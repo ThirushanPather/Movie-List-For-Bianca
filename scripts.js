f65b3e37e55512e0ba3513ec692a662f0f777067
ThirushanPather <114925066+ThirushanPather@users.noreply.github.com>
2024-07-07 20:08:44 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,3 +6,20 @@ function toggleSummary(element) {
     summary.style.display = "block";
   }
 }
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  if (Math.random() < 0.5) {
+    heart.classList.add("pink");
+  }
+  heart.style.left = `${Math.random() * 100}%`;
+  heart.style.animationDuration = `${5 + Math.random() * 3}s`;
+  document.querySelector(".hearts").appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 8000); // Heart lifespan
+}
+
+setInterval(createHeart, 500);
